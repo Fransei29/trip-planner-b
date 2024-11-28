549875ceff19e40feee619d54e9d8609fa701827
Franco Seiler <seilerfranco317@gmail.com>
2024-11-28 10:13:38 -0300

    s

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Title from './components/Title';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-const titleRoot = ReactDOM.createRoot(document.getElementById('title'));
-titleRoot.render(
-  <React.StrictMode>
-    <Title className="title" text="Welcome to your Trip Planner" />
-  </React.StrictMode>
-);
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
